e5e361efa7735e96b314a7e6680acff87e2b91d7
Kelly Asche <kasche@ruralmn.org>
2020-05-14 09:53:23 -0500

    Added new docs folder

---
+++ docs/Data/Case data/cases-1-updated.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+counties.regions <- read_csv("Data/Join docs/county_regions.csv") %>%
+  mutate(countyfp = formatC(countyfp, width = 3, flag = "0"),
+         Name = str_to_title(Name),
+         Name = str_replace(Name, "Q", "q"),
+         Name = str_replace(Name, "Of The", "of the"),
+         Name = str_replace(Name, "Mcleod", "McLeod"),
+         Dem_Desc = ifelse(Name == "Minnesota", "Minnesota", Dem_Desc) ,
+         edr = str_replace(edr, "  ", " "),
+         planning.region = str_replace(planning.region, " Minnesota", ""),
+         planning.region = fct_relevel(planning.region, "Northwest", "Northeast", "Central", "Seven County Mpls-St Paul", "Southwest", "Southeast"),
+         edr = fct_relevel(edr, "EDR 1 - Northwest", "EDR 2 - Headwaters", "EDR 3 - Arrowhead", "EDR 4 - West Central", "EDR 5 - North Central", "EDR 6E- Southwest Central", "EDR 6W- Upper Minnesota Valley", "EDR 7E- East Central", "EDR 7W- Central", "EDR 8 - Southwest", "EDR 9 - South Central", "EDR 10 - Southeast", "EDR 11 - 7 County Twin Cities", "Minnesota"))
+
+
+# Prep cases-1 data -------------------------------------------------------
+
+data <- read_csv("Data/Case data/cases-1.csv") %>%
+  gather(key = "Date", value = "Cases", 3:9) %>%
+  filter(County != "Minnesota")
+
+write_csv(data, "Data/Case data/cases-1-updated.csv")
+
+names(data)
