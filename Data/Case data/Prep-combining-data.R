d9d8de0e3f77d9c32b96f35b8b326e43dc0f9b9c
Kelly Asche <kasche@ruralmn.org>
2020-05-21 18:33:12 -0500

    Added new data 2020-05-21

--- Data/Case data/Prep-combining-data.R
+++ Data/Case data/Prep-combining-data.R
@@ -58,10 +58,16 @@ data.6 <- read_csv("Data/Case data/cases-2020-05-17.csv") %>%
   right_join(counties.regions, by = c("County" = "Name")) %>%
   fill(Date)
 
+data.7 <- read_csv("Data/Case data/cases-2020-05-18-through-21.csv") %>%
+  mutate(County = iconv(County, "UTF-8", "UTF-8",sub=''),
+         Deaths = iconv(Deaths, "UTF-8", "UTF-8", sub = "")) %>%
+  right_join(counties.regions, by = c("County" = "Name")) %>%
+  fill(Date)
+
 # Combine all data --------------------------------------------------------
 
 master.data <- data.2 %>%
-  rbind(data.3,data.4, data.5, data.6) %>%
+  rbind(data.3,data.4, data.5, data.6, data.7) %>%
   left_join(total.pop, by = "countyfp") 
 
 write_csv(master.data, "Data/Case data/Master-cases-county.csv")
